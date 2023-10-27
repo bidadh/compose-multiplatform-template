332cf8b4e4bd0a686b46ff70dff15cb48ed4e448
Arthur Kazemi <bidadh@gmail.com>
2023-10-27 19:45:37 +1100

    add theme

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,11 +1,13 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,27 +17,38 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
+import ui.theme.AppTheme
+
+@Composable
+fun App() {
+  AppTheme {
+    Surface(
+      modifier = Modifier.fillMaxSize(),
+      color = MaterialTheme.colorScheme.background
+    ) {
+      RootScreen()
+    }
+  }
+}
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
-fun App() {
-  MaterialTheme {
-    var greetingText by remember { mutableStateOf("Hello translator app!") }
-    var showImage by remember { mutableStateOf(false) }
-    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-      Button(onClick = {
-        greetingText = "Hello, ${getPlatformName()}"
-        showImage = !showImage
-      }) {
-        Text(greetingText)
-      }
-      TextField(greetingText, onValueChange = { greetingText = it })
-      AnimatedVisibility(showImage) {
-        Image(
-          painterResource("compose-multiplatform.xml"),
-          null
-        )
-      }
+fun RootScreen() {
+  var greetingText by remember { mutableStateOf("Hello translator app!") }
+  var showImage by remember { mutableStateOf(false) }
+  Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+    Button(onClick = {
+      greetingText = "Hello, ${getPlatformName()}"
+      showImage = !showImage
+    }) {
+      Text(greetingText)
+    }
+    TextField(greetingText, onValueChange = { greetingText = it })
+    AnimatedVisibility(showImage) {
+      Image(
+        painterResource("compose-multiplatform.xml"),
+        null
+      )
     }
   }
 }
