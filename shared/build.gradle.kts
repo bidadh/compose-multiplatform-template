9adaf7dce4a41ecf9bb0a149d7ada5687a4278e3
Arthur Kazemi <bidadh@gmail.com>
2023-10-28 12:46:35 +1100

    add version catalogs

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -31,9 +31,9 @@ kotlin {
     val androidMain by getting {
       dependencies {
         implementation(compose.preview)
-        api("androidx.activity:activity-compose:1.7.2")
-        api("androidx.appcompat:appcompat:1.6.1")
-        api("androidx.core:core-ktx:1.10.1")
+        api(libs.activity.compose)
+        api(libs.appcompat)
+        api(libs.core.ktx)
       }
     }
     val iosX64Main by getting
