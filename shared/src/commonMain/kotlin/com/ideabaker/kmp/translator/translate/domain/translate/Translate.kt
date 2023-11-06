528bbca40e34ed1c9c2cc4670fe4f2dd4b0196d8
Arthur Kazemi <bidadh@gmail.com>
2023-11-07 00:02:27 +1100

    add Translate

---
+++ shared/src/commonMain/kotlin/com/ideabaker/kmp/translator/translate/domain/translate/Translate.kt
@@ -0,0 +1,39 @@
+package com.ideabaker.kmp.translator.translate.domain.translate
+
+import com.ideabaker.kmp.translator.core.domain.language.Language
+import com.ideabaker.kmp.translator.core.domain.util.Resource
+import com.ideabaker.kmp.translator.translate.domain.history.HistoryDataSource
+import com.ideabaker.kmp.translator.translate.domain.history.HistoryItem
+
+class Translate(
+  private val client: TranslateClient,
+  private val historyDataSource: HistoryDataSource
+) {
+
+  suspend fun execute(
+    fromLanguage: Language,
+    fromText: String,
+    toLanguage: Language
+  ): Resource<String> {
+    return try {
+      val translatedText = client.translate(
+        fromLanguage, fromText, toLanguage
+      )
+
+      historyDataSource.saveHistory(
+        HistoryItem(
+          id = null,
+          fromLanguageCode = fromLanguage.langCode,
+          fromText = fromText,
+          toLanguageCode = toLanguage.langCode,
+          toText = translatedText,
+        )
+      )
+
+      Resource.Success(translatedText)
+    } catch(e: TranslateException) {
+      e.printStackTrace()
+      Resource.Error(e)
+    }
+  }
+}
