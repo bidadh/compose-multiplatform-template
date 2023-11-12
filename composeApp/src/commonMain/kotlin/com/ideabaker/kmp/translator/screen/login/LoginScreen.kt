792ed45ac0eac9bad5f49b72463f1f30e9a70c85
Arthur Kazemi <bidadh@gmail.com>
2023-11-12 13:20:24 +1100

    add basic login image

--- composeApp/src/commonMain/kotlin/com/ideabaker/kmp/translator/screen/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ideabaker/kmp/translator/screen/login/LoginScreen.kt
@@ -3,6 +3,9 @@ package com.ideabaker.kmp.translator.screen.login
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,6 +13,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
@@ -19,8 +24,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -31,6 +40,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ideabaker.kmm.translator.shared.MR
 import com.ideabaker.kmp.translator.screen.Routes
+import dev.icerock.moko.resources.compose.painterResource
 import dev.icerock.moko.resources.compose.stringResource
 
 @Composable
@@ -45,7 +55,32 @@ fun LoginScreen(
     horizontalAlignment = Alignment.CenterHorizontally
   ) {
 
-    Text(text = stringResource(MR.strings.login_title), style = TextStyle(fontSize = 40.sp))
+    val rainbowColorsBrush = remember {
+      Brush.sweepGradient(
+        listOf(
+          Color(0xFF9575CD),
+          Color(0xFFBA68C8),
+          Color(0xFFE57373),
+          Color(0xFFFFB74D),
+          Color(0xFFFFF176),
+          Color(0xFFAED581),
+          Color(0xFF4DD0E1),
+          Color(0xFF9575CD)
+        )
+      )
+    }
+    Image(
+      painter = painterResource(MR.images.user_login),
+      contentDescription = stringResource(MR.strings.login_title),
+      modifier = Modifier
+        .padding(0.dp, 20.dp, 0.dp, 20.dp)
+        .clip(CircleShape)
+        .border(
+          BorderStroke(4.dp, rainbowColorsBrush),
+          CircleShape
+        )
+        .size(120.dp)
+    )
 
     Spacer(modifier = Modifier.height(20.dp))
     TextField(
