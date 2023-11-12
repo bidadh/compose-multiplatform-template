db40e6a57471ec1ec1962a8218933cbfa77a748a
Arthur Kazemi <bidadh@gmail.com>
2023-11-12 14:03:14 +1100

    add DRAFT Previews

--- composeApp/src/androidMain/kotlin/com/ideabaker/kmp/translator/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/ideabaker/kmp/translator/Previews.kt
@@ -1,25 +1,67 @@
 package com.ideabaker.kmp.translator
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
-import co.touchlab.kermit.Logger
+import androidx.compose.ui.unit.dp
+import com.ideabaker.kmm.translator.shared.MR
+import com.ideabaker.kmp.translator.core.presentation.UiLanguage
 import com.ideabaker.kmp.translator.screen.home.HomeScreen
 import com.ideabaker.kmp.translator.screen.home.HomeViewModel
 import com.ideabaker.kmp.translator.screen.login.LoginScreen
 import com.ideabaker.kmp.translator.screen.login.LoginState
-import com.ideabaker.kmp.translator.screen.login.LoginViewModel
 import com.ideabaker.kmp.translator.screen.pin.PinScreen
 import com.ideabaker.kmp.translator.service.GreetingService
+import com.ideabaker.kmp.translator.translate.presentation.components.LanguageDropDownItem
+import dev.icerock.moko.resources.compose.painterResource
+import dev.icerock.moko.resources.compose.stringResource
 
+
+@Preview
+@Composable
+fun ImagePreview() {
+  val imageModifier = Modifier
+    .size(150.dp)
+    .border(BorderStroke(1.dp, Color.Black))
+    .background(Color.Yellow)
+  Image(
+    painter = painterResource(MR.images.user_login),
+    contentDescription = stringResource(MR.strings.login_title),
+    contentScale = ContentScale.Fit,
+    modifier = imageModifier
+  )
+}
+
+
+@Preview
+@Composable
+fun LanguageDropDownItemPreview() {
+  LanguageDropDownItem(
+    language = UiLanguage.by("en"),
+    onClick = { }
+  )
+}
 
 @Preview(
   name = "Login Screen",
   showSystemUi = true,
   showBackground = false,
 )
+@Preview(
+  device = "spec:parent=pixel_5,orientation=landscape"
+
+)
 @Composable
 fun LoginScreenPreview() {
-  LoginScreen(LoginViewModel(Logger.withTag("LoginViewModel"))) {}
+  val state = LoginState()
+  LoginScreen(state, {}) {}
 }
 
 @Preview(
